945896aeabce24b61b41876b9836562f826c7faa
JoshZhuang <JoshZhuang@192.168.1.15>
2021-02-20 21:30:42 +1030

    passing paramemters from pom to test case

--- automationdemo.maven/src/test/java/automationdemo/maven/demo/HelloWorldMaven.java
+++ automationdemo.maven/src/test/java/automationdemo/maven/demo/HelloWorldMaven.java
@@ -1,12 +1,14 @@
 package automationdemo.maven.demo;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class HelloWorldMaven {
 
+	@Parameters({"param1","param2"})
 	@Test
-	public void testHello() {
+	public void testHello(String text1,String text2) {
 		
-		System.out.println("hello maven world");
+		System.out.println("$$$$$$$$HELLO MAVEN WORLD$$$$$$$"+text1+" "+text2);
 	}
 }
