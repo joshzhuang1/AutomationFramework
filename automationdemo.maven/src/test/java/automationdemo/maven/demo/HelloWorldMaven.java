798e5107e3470765401c2e0393bac552580eb1d4
JoshZhuang <JoshZhuang@192.168.1.15>
2021-02-20 10:51:49 +1030

    configured pom.xml and testng.xml ----- for maven demo

---
+++ automationdemo.maven/src/test/java/automationdemo/maven/demo/HelloWorldMaven.java
@@ -0,0 +1,12 @@
+package automationdemo.maven.demo;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldMaven {
+
+	@Test
+	public void testHello() {
+		
+		System.out.println("hello maven world");
+	}
+}
