f29328ee9096d5c0f747ad760772b705dd982704
joshzhuang1 <79257166+joshzhuang1@users.noreply.github.com>
2021-02-26 16:58:48 +1030

    initial version BDD cumcumber

---
+++ cumdemo.maven/src/main/java/cumdemo/maven/cumdemo/maven/App.java
@@ -0,0 +1,13 @@
+package cumdemo.maven.cumdemo.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
